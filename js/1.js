af34fefeea02445eb81436d7d2adc6b63f6826cc
Harshvardhan Rao <51167811+plantedbrain@users.noreply.github.com>
2021-03-19 12:46:47 +0530

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,10 @@
+<script src='js/sweetalert2.min.js' />;
+<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>;
+<script type='text/javascript'>
+    swal({
+    title: 'Good job!',
+    text: 'You clicked the button!',
+    icon: 'success',
+    button: 'Aww yiss!',
+  });
+</script>
---
+++ js/home_1.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent : document.querySelector(".pic"),
+    intensity : 0.2,
+    image1 : "img/camera.png",
+    image2 : "img/1.jpg"
+    displacementImage : "img/123.png"
+
+});
